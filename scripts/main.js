6bc4c483b1dd3e22f9b97c06c7134f9139e27349
Julianto Armando <blade.royalty@gmail.com>
2024-05-01 14:57:18 +0700

    Final sebelum Responsive test

---
+++ scripts/main.js
@@ -0,0 +1,20 @@
+// clear before unload
+window.onbeforeunload = () => {
+  for (const form of document.getElementsByTagName("form")) {
+    form.reset();
+  }
+};
+
+// Togle And Responsive Navigation
+
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const navLists = document.querySelector("nav");
+
+  burger.addEventListener("click", () => {
+    navLists.classList.toggle("nav-active");
+    burger.classList.toggle("toggle-burger");
+  });
+};
+
+navSlide();
